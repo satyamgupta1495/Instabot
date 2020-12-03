4c76110411a70fef02e3e61fa9b6ce8f38ee0e1e
Satyam <satyamgupta1495@gmail.com>
2020-12-03 19:44:53 +0530

    Driver and Path Updated

--- bot.py
+++ bot.py
@@ -6,63 +6,39 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 def login():
-    chromedriver = "D:\\Selenium Driver\\chromedriver.exe"
+    chromedriver = "D:\\Drivers\\chromedriver.exe"
     driver = webdriver.Chrome(chromedriver)
     driver.get("https://instagram.com")
-    driver.implicitly_wait(5)
-
-    username = driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")
+    driver.implicitly_wait(10)
+    username = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
     username.send_keys("")
-    password = driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")
+    password = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
     password.send_keys("")
 
-    login = driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div")
+    login = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]")
     login.click()
 
-    notnow = driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/div/div/div/div/button")
+    notnow = driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button")
     notnow.click()
 
-    # fb = driver.find_element_by_xpath(
-    #     "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[6]/button")
-    # fb.click()
 
-    # username = driver.find_element_by_xpath(
-    #     "/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[1]/input")
-    # username.send_keys("8149701657")
-    # password = driver.find_element_by_xpath(
-    #     "/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[2]/input")
-    # password.send_keys("satyam&justin")
-
-    # login_button = driver.find_element_by_xpath(
-    #     "/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[3]/button")
-    # login_button.click()
-
-    skip = driver.find_element_by_xpath(
-        "/html/body/div[4]/div/div/div/div[3]/button[2]")
+    skip = driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]")
     skip.click()
 
-    search = driver.find_element_by_xpath(
-        "/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input")
-    search.send_keys("#desimeme")
+    search = driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input")
+    search.send_keys("#poems")
 
-    animeart = driver.find_element_by_xpath(
-        "/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[11]/div/div/div[1]/span")
+    animeart = driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[4]/div/a[1]/div")
     animeart.click()
 
     likeandcomment(driver)
 
 
 def likeandcomment(driver):
-    driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div/div[8]/div[3]/a/div").click()
+    driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div/div[8]/div[3]/a/div").click()
     while True:
         time.sleep(2)
-        post = driver.find_element_by_xpath(
-            "/html/body/div[1]/section/main/article/div[2]/div/div[8]/div[3]/a/div")
+        post = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div/div[8]/div[3]/a/div")
         actionChains1 = ActionChains(driver)
         actionChains1.double_click(post).perform()
         # select comment input
